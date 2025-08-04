9d21e8b844a4af29f982f1d45363539e074759b9
Mridu20 <mriduixb7454@gmail.com>
2025-08-04 10:39:51 +0530

    Time: 3 ms (45.79%), Space: 10.8 MB (20.93%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -4,6 +4,10 @@ public:
         bool h = true;
         string s = to_string(x);
 
+        if(x < 0){
+            return false;
+        }
+
         for(int i=0; i< s.size()/2; i++){
             if(s[i] != s[s.size()-1-i]){
                 h = false;
