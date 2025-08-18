c3874c0fac540dd1ab6662c70230c302eda00c22
Mridu20 <mriduixb7454@gmail.com>
2025-08-18 21:36:05 +0530

    Time: 4 ms (29.29%), Space: 16.6 MB (15.58%) - LeetHub

--- 1807-partitioning-into-minimum-number-of-deci-binary-numbers/1807-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
+++ 1807-partitioning-into-minimum-number-of-deci-binary-numbers/1807-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
@@ -1,11 +1,7 @@
 class Solution {
 public:
     int minPartitions(string n) {
-        vector<int> v;
-        for (char c : n) {
-            v.push_back(c - '0'); 
-        }
-        int l= *max_element(v.begin(), v.end()); 
-        return l;
+        int l= *max_element(n.begin(), n.end()); 
+        return l- '0';
     }
 };
