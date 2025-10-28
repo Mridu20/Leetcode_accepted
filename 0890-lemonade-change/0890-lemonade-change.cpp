1d49fd08841e017fa57cdd9395d7efba7961e6d9
Mridu20 <mriduixb7454@gmail.com>
2025-10-28 22:38:03 +0530

    Time: 58 ms (3.05%), Space: 87.3 MB (8.83%) - LeetHub

---
+++ 0890-lemonade-change/0890-lemonade-change.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& a) {
+        int cnt5 = 0, cnt10=0, cnt20=0;
+
+        for(int i=0; i<a.size(); i++){
+            if(a[i]==10){
+                cout << 0 <<" ";
+                if(cnt5 == 0)return false;
+                else cnt5--;
+            }
+            else if(a[i]==20){
+                cout << 1 <<" ";
+                if(cnt5 >= 1 && cnt10 >= 1){
+                    cnt5--;
+                    cnt10--;
+                }
+                else if(cnt5 >= 3) cnt5 -=3;
+                else return false;
+            }
+
+            if(a[i]==5)cnt5++;
+            else if(a[i]==10)cnt10++;
+            else cnt20++;
+        }
+        return true;
+    }
+};
