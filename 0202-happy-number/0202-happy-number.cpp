00a86716f9e754d8b0c692d83c04184faa461b76
Mridu20 <mriduixb7454@gmail.com>
2025-12-11 13:52:33 +0530

    Time: 1 ms (30.61%), Space: 8.3 MB (53.02%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        unordered_set<int> usedIntegers;
+        while (true) {
+            int sum = 0;
+            int temp = n;
+
+            while (temp != 0) {
+                int digit = temp % 10;
+                sum += digit * digit;
+                temp /= 10;
+            }
+
+            if (sum == 1)
+                return true;
+
+            n = sum;
+
+            if (usedIntegers.count(n))
+                return false; 
+
+            usedIntegers.insert(n);
+        }
+    }
+};
