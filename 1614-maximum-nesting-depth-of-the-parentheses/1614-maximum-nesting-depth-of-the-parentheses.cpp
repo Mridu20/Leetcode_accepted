80f31bb02217ca73fd31d56cbda4178b74c4a22f
Mridu20 <mriduixb7454@gmail.com>
2026-03-02 15:40:34 +0530

    Time: 0 ms (100%), Space: 8.3 MB (84.98%) - LeetHub

---
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        int cnt=0;
+        int ans = -1;
+        for(auto &c : s){
+            if(c == '(')cnt++;
+            else if(c == ')')cnt--;
+
+            ans = max(ans, cnt);
+        }
+
+        return ans;
+    }
+};
