e956b75f5e7458e53774c474116249c150dbb881
Mridu20 <mriduixb7454@gmail.com>
2026-03-16 18:56:40 +0530

    Time: 8 ms (83.1%), Space: 11.9 MB (22.19%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -12,13 +12,11 @@ public:
         ListNode* temp1 = head;
         ListNode* temp2 = head;
 
-        while(temp2 != NULL && temp2->next != NULL){
+        while(temp2 && temp2->next){
             temp1 = temp1->next;
             temp2 = temp2->next->next;
-
             if(temp1 == temp2)return true;
         }
-
         return false;
     }
 };
